bcb89239df0df79b6a8671c23accc9443764f5d9
vivify <vivify@vivifys-MacBook-Air.local>
2022-05-20 21:14:48 +0200

    evo ga

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CarController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,3 +22,5 @@ Route::get('/', function () {
 Route::get('/about-us', function (){
     return view('about-us');
 });
+
+Route::get('/cars', [CarController::class, 'index']);
